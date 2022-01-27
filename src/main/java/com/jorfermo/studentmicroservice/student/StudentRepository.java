2c6cf7bee0ec1fa6783f3847364fe5c9eab8450f
Jorge Ferrando <jorge.ferrando@crowdhouse.ch>
2022-01-27 21:53:48 +0100

    feat: create student

--- src/main/java/com/jorfermo/studentmicroservice/student/StudentRepository.java
+++ src/main/java/com/jorfermo/studentmicroservice/student/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.jorfermo.studentmicroservice.student;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+    Optional<Student> findStudentByEmail(String email);
 }
